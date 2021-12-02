c558412bd79bc91db1720a4ead3213aa20be0917
mdimranh <imran15-1984@diu.edu.bd>
2021-12-03 00:11:15 +0600

    Initial aommit

---
+++ firstapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('first/', firstAPI),
+    path('registration/', registerAPI),
+]
---
+++ firstapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from django.contrib.auth.models import User
+
+@api_view(['GET', 'POST'])
+def firstAPI(request):
+    if request.method=="POST":
+        name = request.data['name']
+        age = request.data['age']
+        print(name, age)
+        return Response({'name': name, 'age': age})
+    context={
+        'name': 'Imran Hossain',
+        'university': 'DIU'
+    }
+    return Response(context)
+
+
+@api_view(['POST',])
+def registerAPI(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if User.objects.filter(username = username).exists():
+            return Response({'error': 'This usernamne is already exists!'})
+        elif password1 != password2:
+            return Response({'error':"Both password didn't match!"})
+
+        user = User(
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            email = email
+        )
+        user.set_password(password1)
+        user.save()
+        return Response({'success': 'Registration successfull'})

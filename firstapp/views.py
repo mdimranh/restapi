e43d1215ae5165b238824980437daaf8d97a68ce
mdimranh <imran15-1984@diu.edu.bd>
2021-12-06 16:59:18 +0600

    views.py modified

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
+from rest_framework import 
+
 from rest_framework.permissions import AllowAny, IsAuthenticated
 # from rest_framework.authentication import TokenAuthentication
 
